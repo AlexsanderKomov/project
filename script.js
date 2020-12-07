e030480509c8e658a5aac3003f9719e00b9cc90b
Sasha <sasha.komov.93@mail.ru>
2020-12-07 20:25:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+console.log(4 / 0);
+
+let string = "Hello";
+
+console.log(string);
+
+
+let persone = {
+   name: "Jonh",
+   age: 25,
+   isMarried: true
+};
+
+console.log(persone["name"]);
+
+let arr = ["plum.png", "orange.jpg", "app.bmp"];
+
+console.log(arr[1]);
+
+console.log(4 + ' string');
+
+let one = true;
+let tu = true;
+let fo = true;
+
+console.log(0 || 5 && false);
+
+
